50a0cf675740eea4e9afdefd15a7b24ef392fa89
Radu <radu.jica+Code@gmail.com>
2018-02-09 15:29:54 +0100

    Update converter

--- weld/converter.py
+++ weld/converter.py
@@ -4,39 +4,51 @@ import pandas as pd
 import numpy as np
 
 """
-Convert NETCDF3_CLASSIC file from netCDF4 to pandas DataFrame.
-Based on how xarray does it.
-
-TODO: 
-- generalize datetime format conversion, i.e. the =='time' check
-- xarray seems to round(?) or just convert from float32 to float64
+Used to convert my relevant netCDF3_CLASSIC datasets to pandas dataframes.
 """
 
-PATH = '/export/scratch1/radujica/datasets/ECAD/original/small_sample/tg.nc'
 
+def to_dataframe(ds):
+    """ Convert netCDF4.Dataset to pandas.DataFrame
 
-def to_dataframe(path):
-    ds = netCDF4.Dataset(path, format='NECDF3_CLASSIC')
+    Based on how xarray does it.
+
+    TODO:
+
+    - further generalize datetime format conversion; can calendar attr be in non-datetime data?
+
+    - xarray seems to round(?) or just convert from float32 to float64
+
+    Parameters
+    ----------
+    ds : netCDF4.Dataset
+        to convert from
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        the resulting dataframe
+
+    """
+    
     columns = [k for k in ds.variables if k not in ds.dimensions]
-    ordered_dims = dict(map(lambda kv: (kv[0], kv[1].size), OrderedDict(sorted(ds.dimensions.items())).items()))
+    ordered_dimensions = dict(map(lambda kv: (kv[0], kv[1].size), OrderedDict(sorted(ds.dimensions.items())).items()))
 
     data = []
     for k in columns:
         variable_dims = ds.variables[k].dimensions
-        expanded_dims = tuple(d for d in ordered_dims if d not in set(variable_dims)) + variable_dims
         raw_data = ds.variables[k][0:]
-        expanded_data = np.broadcast_to(raw_data.filled(np.nan), tuple(ordered_dims[d] for d in expanded_dims))
-        axes = tuple(expanded_dims.index(d) for d in ordered_dims)
+        shape = tuple(ordered_dimensions[d] for d in variable_dims)
+        expanded_data = np.broadcast_to(raw_data.filled(np.nan), shape)
+        axes = tuple(variable_dims.index(d) for d in ordered_dimensions)
         transposed_data = np.transpose(expanded_data, axes)
         data.append(transposed_data.reshape(-1))
 
     def convert_datetime(variable):
         return pd.to_datetime(netCDF4.num2date(variable[0:], variable.units, calendar=variable.calendar))
 
-    names = ordered_dims
-    indexes = [convert_datetime(ds.variables[k]) if k == u'time' else ds.variables[k][0:] for k in names]
-    index = pd.MultiIndex.from_product(indexes, names=names)
+    indexes = [convert_datetime(ds.variables[k]) if hasattr(ds.variables[k], 'calendar')
+               else ds.variables[k][0:] for k in ordered_dimensions]
+    index = pd.MultiIndex.from_product(indexes, names=ordered_dimensions)
+
     return pd.DataFrame(OrderedDict(zip(columns, data)), index=index)
-
-
-print(to_dataframe(PATH))
