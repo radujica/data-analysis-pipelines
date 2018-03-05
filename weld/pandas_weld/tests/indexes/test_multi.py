46f8fe8c9dbd43112ef4d2c2ea393ff9dce71910
Radu <radu.jica+Code@gmail.com>
2018-03-05 10:40:20 +0100

    Add tests with LazyData

--- weld/pandas_weld/tests/indexes/test_multi.py
+++ weld/pandas_weld/tests/indexes/test_multi.py
@@ -1,6 +1,5 @@
 import unittest
 import numpy as np
-from weld.types import WeldLong, WeldVec
 from pandas_weld.core.indexes.multi import cartesian_product_indices, duplicate_elements_indices, \
     duplicate_array_indices
 
@@ -91,16 +90,67 @@ class CartesianProductTests(unittest.TestCase):
                                       "Second array result is incorrect")
 
     @staticmethod
-    def test_correct_weld_object_input_first():
-        pass
+    def test_correct_lazy_data_input_first():
+        array1_dtype = np.dtype(np.int32)
+        array1 = np.array([1, 2, 3], dtype=array1_dtype)
+        array1 = duplicate_elements_indices(array1, 2L)
+        array2_dtype = np.dtype(np.int32)
+        array2 = np.array([4, 5], dtype=array2_dtype)
+
+        result = cartesian_product_indices([array1, array2])
+
+        expected_array1_result = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], dtype=np.int64)
+        expected_array2_result = np.array([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], dtype=np.int64)
+
+        np.testing.assert_array_equal(result[0].evaluate(verbose=False),
+                                      expected_array1_result,
+                                      "First array result is incorrect")
+        np.testing.assert_array_equal(result[1].evaluate(verbose=False),
+                                      expected_array2_result,
+                                      "Second array result is incorrect")
 
     @staticmethod
-    def test_correct_weld_object_input_second():
-        pass
+    def test_correct_lazy_data_input_second():
+        array1_dtype = np.dtype(np.int32)
+        array1 = np.array([1, 2, 3], dtype=array1_dtype)
+        array2_dtype = np.dtype(np.int32)
+        array2 = np.array([4, 5], dtype=array2_dtype)
+        array2 = duplicate_elements_indices(array2, 2L)
+
+        result = cartesian_product_indices([array1, array2])
+
+        expected_array1_result = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], dtype=np.int64)
+        expected_array2_result = np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3], dtype=np.int64)
+
+        np.testing.assert_array_equal(result[0].evaluate(verbose=False),
+                                      expected_array1_result,
+                                      "First array result is incorrect")
+        np.testing.assert_array_equal(result[1].evaluate(verbose=False),
+                                      expected_array2_result,
+                                      "Second array result is incorrect")
 
     @staticmethod
-    def test_correct_weld_object_input_both():
-        pass
+    def test_correct_lazy_data_input_both():
+        array1_dtype = np.dtype(np.int32)
+        array1 = np.array([1, 2, 3], dtype=array1_dtype)
+        array1 = duplicate_elements_indices(array1, 2L)
+        array2_dtype = np.dtype(np.int32)
+        array2 = np.array([4, 5], dtype=array2_dtype)
+        array2 = duplicate_elements_indices(array2, 2L)
+
+        result = cartesian_product_indices([array1, array2])
+
+        expected_array1_result = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+                                          dtype=np.int64)
+        expected_array2_result = np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3],
+                                          dtype=np.int64)
+
+        np.testing.assert_array_equal(result[0].evaluate(verbose=False),
+                                      expected_array1_result,
+                                      "First array result is incorrect")
+        np.testing.assert_array_equal(result[1].evaluate(verbose=False),
+                                      expected_array2_result,
+                                      "Second array result is incorrect")
 
 
 class DuplicateElementsTests(unittest.TestCase):
@@ -109,6 +159,20 @@ class DuplicateElementsTests(unittest.TestCase):
         array_dtype = np.dtype(np.int32)
         array = np.array([1, 2, 3], dtype=array_dtype)
         n = 4L
+
+        result = duplicate_elements_indices(array, n)
+
+        expected_array_result = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], dtype=np.int64)
+
+        np.testing.assert_array_equal(result.evaluate(verbose=False),
+                                      expected_array_result)
+
+    @staticmethod
+    def test_correct_lazy_data_input():
+        array_dtype = np.dtype(np.int32)
+        array = np.array([1, 2, 3], dtype=array_dtype)
+        array = duplicate_elements_indices(array, 2L)
+        n = 2L
 
         result = duplicate_elements_indices(array, n)
 
@@ -128,6 +192,20 @@ class DuplicateArrayTests(unittest.TestCase):
         result = duplicate_array_indices(array, n)
 
         expected_array_result = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2], dtype=np.int64)
+
+        np.testing.assert_array_equal(result.evaluate(verbose=False),
+                                      expected_array_result)
+
+    @staticmethod
+    def test_correct_lazy_data_input():
+        array_dtype = np.dtype(np.int32)
+        array = np.array([1, 2, 3], dtype=array_dtype)
+        array = duplicate_array_indices(array, 2L)
+        n = 2L
+
+        result = duplicate_array_indices(array, n)
+
+        expected_array_result = np.array([0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5], dtype=np.int64)
 
         np.testing.assert_array_equal(result.evaluate(verbose=False),
                                       expected_array_result)
