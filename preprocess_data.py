1018b1d8ce2c08b9ea028a882b5807784eeda47f
Radu <radu.jica+Code@gmail.com>
2018-04-23 11:47:43 +0200

    Update preprocessing script

--- preprocess_data.py
+++ preprocess_data.py
@@ -7,26 +7,31 @@ Join and convert the NETCDF3_CLASSIC files into a large NETCDF4 file (with full 
 Note: this uses the same dependencies as in python-libraries
 """
 
-PATH_HOME = os.getenv('HOME', '/')
-PATH_DATASETS_ROOT = '/datasets/ECAD/original/sample/'
-FILES = ['tg', 'tg_err', 'tn', 'tn_err', 'tx', 'tx_err', 'pp', 'pp_err', 'rr', 'rr_err']
-PATH_EXTENSION = '.nc/data'
-OUTPUT_NAME = 'combined.nc'
+PATH_HOME = os.getenv('HOME2')
+PATH_DATASETS_ROOT = '/datasets/ECAD/original/small_sample/'
 
-# first dataset
-result_ds = xr.open_dataset(PATH_HOME + PATH_DATASETS_ROOT + FILES[0] + PATH_EXTENSION)
 
-# merge all into result_ds
-for file_name in FILES[1:]:
-    raw_ds = xr.open_dataset(PATH_HOME + PATH_DATASETS_ROOT + file_name + PATH_EXTENSION)
+def combine(path, files, output):
+    file_extension = '.nc'
 
-    # the _err files have the same variable name as the non _err files, so fix it
-    # e.g. tg -> tg_err
-    if FILES.index(file_name) % 2 == 1:
-        raw_ds.rename({FILES[FILES.index(file_name) - 1]: file_name}, inplace=True)
+    # first dataset
+    result_ds = xr.open_dataset(path + files[0] + file_extension)
 
-    result_ds = xr.merge([result_ds, raw_ds], join='inner')
+    # merge all into result_ds
+    for file_name in files[1:]:
+        raw_ds = xr.open_dataset(path + file_name + file_extension)
 
-result_ds.to_netcdf(path=PATH_HOME + PATH_DATASETS_ROOT + OUTPUT_NAME,
-                    format='NETCDF4',
-                    engine='netcdf4')
+        # the _err files have the same variable name as the non _err files, so fix it
+        # e.g. tg -> tg_err
+        if files.index(file_name) % 2 == 1:
+            raw_ds.rename({files[files.index(file_name) - 1]: file_name}, inplace=True)
+
+        result_ds = xr.merge([result_ds, raw_ds], join='inner')
+
+    result_ds.to_netcdf(path=path + output + file_extension,
+                        format='NETCDF4',
+                        engine='netcdf4')
+
+
+combine(PATH_HOME + PATH_DATASETS_ROOT, ['tg', 'tg_err', 'pp', 'pp_err', 'rr', 'rr_err'], 'data1')
+combine(PATH_HOME + PATH_DATASETS_ROOT, ['tn', 'tn_err', 'tx', 'tx_err'], 'data2')
