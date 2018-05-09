a90ebe8e3087e125be86f777638ac6cc533222b0
Radu <radu.jica+Code@gmail.com>
2018-05-09 14:56:04 +0200

    Implement Spark pipeline v1

--- spark/build.sbt
+++ spark/build.sbt
@@ -3,6 +3,8 @@ scalaVersion := "2.11.8"
 name := "spark"
 version := "1.0"
 
+fork in run := true
+
 resolvers += "Unidata maven repository" at "http://artifacts.unidata.ucar.edu/content/repositories/unidata-releases"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.0"
 libraryDependencies += "edu.ucar" % "netcdf" % "4.3.22"
--- spark/src/main/scala/Main.scala
+++ spark/src/main/scala/Main.scala
@@ -3,12 +3,14 @@ import java.io.IOException
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.{FloatType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.{udf, abs}
 import ucar.nc2.time.CalendarDate
 import ucar.nc2.{NetcdfFile, Variable}
 
 import collection.JavaConversions._
 
+
+// TODO: fat jar http://blog.prabeeshk.com/blog/2014/04/08/creating-uber-jar-for-spark-project-using-sbt-assembly/
 object Main {
   private val PATH: String = "/export/scratch1/radujica/datasets/ECAD/original/small_sample/"
   //private val PATH: String = System.getenv("HOME2") + "/datasets/ECAD/original/small_sample/"
@@ -91,12 +93,16 @@ object Main {
   }
 
   // TODO: generalize this
+  // TODO: create rdd for each column and union? this avoids having all the data in driver memory at once
+  // but shuffles data to workers n columns times; 1) do we have setup small cluster to use? pointless otherwise
+  // 2. is transpose more or less costly than this?
   @throws[IOException]
-  private def readData(path: String, ss: SparkSession, size: Int): DataFrame = {
+  private def readData(path: String, ss: SparkSession, size: Int, addIndex: Boolean): DataFrame = {
     val file = NetcdfFile.open(path)
-    //var data: mutable.LinkedHashMap[String, Array[Float]] = new mutable.LinkedHashMap[String, Array[Float]]()
-    val data: Array[Array[Float]] = new Array[Array[Float]](size)
-    val names: Array[String] = new Array[String](size)
+    var sizeF = size
+    if (addIndex) sizeF += 1
+    val data: Array[Array[Float]] = new Array[Array[Float]](sizeF)
+    val names: Array[String] = new Array[String](sizeF)
     var i = 0
     for (variable : Variable <- file.getVariables) {
       data(i) = readVariable(variable)
@@ -112,11 +118,17 @@ object Main {
     data(names.indexOf("latitude")) = dimensions(1)
     data(names.indexOf("time")) = dimensions(2)
 
+    // add index column (needed for subset by row numbers)
+    if (addIndex) {
+      data(i) = Array.tabulate(data(0).length)(_ + 1)
+      names(i) = "index"
+    }
+
     val rdd: RDD[Row] = ss.sparkContext.parallelize(data.transpose.toSeq.map(x => Row.fromSeq(x.toSeq)))
 
     val df: DataFrame = ss.createDataFrame(rdd, StructType(names.map(StructField(_, FloatType, nullable = false)).toSeq))
 
-    // convert time from (float) to String
+    // convert time from float to String
     val floatTimeToString = udf((time: Float) => {
       val udunits = String.valueOf(time.asInstanceOf[Int]) + " " + UNITS
 
@@ -128,14 +140,65 @@ object Main {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder
       .appName("Spark Pipeline")
-      .master("local")
-      .config("spark.executor.heartbeatInterval", 20)
+      .master("local[*]")  // local or local[4]
+      //.config("spark.executor.heartbeatInterval", 20)
+      .config("spark.executor.memory", "2g")
+      .config("spark.driver.memory", "16g")
+      .config("spark.driver.maxResultSize", "10g")
       .getOrCreate()
 
-    val df1: DataFrame = readData(PATH + "data1.nc", spark, 9)
-    val df2: DataFrame = readData(PATH + "data2.nc", spark, 7)
+    // TODO: optimize partitioning ~ same partitioner in paralellize
+    val df1: DataFrame = readData(PATH + "data1.nc", spark, 9, addIndex = true)
+    val df2: DataFrame = readData(PATH + "data2.nc", spark, 7, addIndex = false)
 
-    val df = df1.join(df2, Seq("longitude", "latitude", "time"), "inner")
+    // PIPELINE
+    // 1. join the 2 dataframes
+    var df: DataFrame = df1.join(df2, Seq("longitude", "latitude", "time"), "inner")
+
+    // 2. quick preview on the data
+    println(df.show(10))
+
+    // 3. subset the data
+    // the only way to select by row number; more effectively would be to just filter by latitude as intended, though
+    // this deviates even further from the other pipeline implementations
+    df = df.filter(df("index") >= 709920.0f && df("index") < 1482480.0f)
+
+    // 4. drop rows with null values
+    df = df.filter(df("tg") =!= -99.99f && df("pp") =!= -999.9f && df("rr") =!= -999.9f)
+
+    // 5. drop columns
+    df = df.drop("pp_err", "rr_err", "index")
+
+    // 6. UDF 1: compute absolute difference between max and min
+    df = df.withColumn("abs_diff", abs(df("tx") - df("tn"))).cache()
+
+    // 7. explore the data through aggregations
+    println(df.drop("longitude", "latitude", "time").describe().show())
+    // describe also computes count but presumably the aggregations computation is optimized;
+    // doing the aggregations separately (as below) is more costly
+//    val aggregations = Seq("min", "max", "mean", "std")
+//    val columnsToAgg = df.drop("longitude", "latitude", "time").columns
+//    for (aggregation: String <- aggregations) {
+//      val aggr_map: Map[String, String] = columnsToAgg.map(column => column -> aggregation).toMap
+//      println(df.agg(aggr_map).show())
+//    }
+
+    // 8. compute mean per month
+    // UDF 2: compute custom year+month format
+    val computeYearMonth = udf((time: String) => {
+      time.substring(0, 7).replace("-", "")
+    })
+    df = df.withColumn("year_month", computeYearMonth(df("time")))
+    // group by
+    val columnsToAgg: Array[String] = Array("tg", "tn", "tx", "pp", "rr")
+    val groupOn: Seq[String] = Seq("longitude", "latitude", "year_month")
+    val grouped: DataFrame = df.groupBy(groupOn.head, groupOn.drop(1): _*).agg(columnsToAgg.map(column => column -> "mean").toMap)
+    // join
+    df = df.join(grouped, groupOn)
+    df = df.drop("year_month")
+
+    // final evaluate
+    println(df.collect())
 
     spark.stop()
   }
