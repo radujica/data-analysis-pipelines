83a46ea7191b7da3173751b0da880568cf350e79
Radu <radu.jica+Code@gmail.com>
2018-06-07 11:59:35 +0200

    Fix bugged subset generation

--- benchmarking/prepare_data.sh
+++ benchmarking/prepare_data.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# i.e, prepare the following subsets
-# 1950-01-01    1951-01-16  12.5625m    1.046y  1.5625%
-# 1950-01-01    1952-02-04  25.125m     2.093y  3.125%
-# 1950-01-01    1954-03-07  50.25m      4.187y  6.25%
-# 1950-01-01    1958-05-16  100.5m      8.37y   12.5%
-# 1950-01-01    1963-04-30  201m        13.3y   25%
-# 1950-01-01    1983-06-30  402m        33.5y   50%
-# 1950-01-01    2017-12-31  804m        67y     100%
+# i.e, prepare the following subsets; percentage is approximate as dates were calculated
+# based on months, not days per year, resulting in actual percentage being slightly less than shown
+# 1950-01-01    1951-01-16  12.5625m    1.046y  1.5625%     381days
+# 1950-01-01    1952-02-04  25.125m     2.093y  3.125%      765days
+# 1950-01-01    1954-03-07  50.25m      4.187y  6.25%       1527days
+# 1950-01-01    1958-05-16  100.5m      8.37y   12.5%       3058days
+# 1950-01-01    1963-04-30  201m        13.3y   25%         4868days
+# 1950-01-01    1983-06-30  402m        33.5y   50%         12234days
+# 1950-01-01    2017-12-31  804m        67y     100%        24837days
 
 # unzip all, select required subsets and create required data1.nc and data2.nc for each subset
+# there are 201 unique latitude, 464 unique longitude, and x unique days depending on subset; lat 42.375 is at index 68
+# and lat 60.125 is at index 139; therefore, to compute the slice required: 68 * 464 * <n_days> and 140 * 464 * <n_days>
 
 path=${HOME2}
 # all files should be downloaded from the E-OBS dataset v17 into data_100;
@@ -33,10 +36,10 @@ declare -a outputs=("${output}data_1/"
                     "${output}data_50/")
 start="1950-01-01"
 declare -a stops=("1951-01-16"
-                  "1950-02-04"
-                  "1956-03-07"
-                  "1963-05-16"
-                  "1983-04-30"
+                  "1952-02-04"
+                  "1954-03-07"
+                  "1958-05-16"
+                  "1963-04-30"
                   "1983-06-30")
 
 for ((i=0; i<${#outputs[@]}; i++));
